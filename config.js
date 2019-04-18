a05da82135b93d9f571737365da0cb01e66bc7b2
Nirmal Rijal <nirmalrijal41@gmail.com>
2019-04-18 18:42:38 +0545

    Add data for : 2019-04-17 - PM10

--- config.js
+++ config.js
@@ -31,6 +31,204 @@ const dataSources = [
         code: 780
       }
     ]
+  },
+  {
+    name: "dhulikhel-kavre",
+    features: [
+      {
+        name: "PM10",
+        code: 1254
+      },
+      {
+        name: "PM2.5",
+        code: 1256
+      },
+      {
+        name: "Wind Speed",
+        code: 1257
+      },
+      {
+        name: "Wind Direction",
+        code: 1258
+      },
+      {
+        name: "Total Suspended Particulate",
+        code: 1263
+      },
+      {
+        name: "Air Temperature",
+        code: 1264
+      },
+      {
+        name: "PM1",
+        code: 1255
+      }
+    ]
+  },
+  {
+    name: "sauraha-chitwan",
+    features: [
+      {
+        name: "PM10",
+        code: 189
+      },
+      {
+        name: "PM2.5",
+        code: 195
+      },
+      {
+        name: "Wind Speed",
+        code: 198
+      },
+      {
+        name: "Wind Direction",
+        code: 199
+      },
+      {
+        name: "Total Suspended Particulate",
+        code: 211
+      },
+      {
+        name: "Air Temperature",
+        code: 212
+      },
+      {
+        name: "PM1",
+        code: 192
+      }
+    ]
+  },
+  {
+    name: "baisipati-ktm",
+    features: [
+      {
+        name: "PM10",
+        code: 523
+      },
+      {
+        name: "PM2.5",
+        code: 525
+      },
+      {
+        name: "Wind Speed",
+        code: 526
+      },
+      {
+        name: "Wind Direction",
+        code: 527
+      },
+      {
+        name: "Total Suspended Particulate",
+        code: 534
+      },
+      {
+        name: "Air Temperature",
+        code: 535
+      },
+      {
+        name: "PM1",
+        code: 524
+      }
+    ]
+  },
+  {
+    name: "pulchowk-lalitpur",
+    features: [
+      {
+        name: "PM10",
+        code: 92
+      },
+      {
+        name: "PM2.5",
+        code: 98
+      },
+      {
+        name: "Wind Speed",
+        code: 101
+      },
+      {
+        name: "Wind Direction",
+        code: 102
+      },
+      {
+        name: "Total Suspended Particulate",
+        code: 114
+      },
+      {
+        name: "Air Temperature",
+        code: 115
+      },
+      {
+        name: "PM1",
+        code: 95
+      }
+    ]
+  },
+  {
+    name: "jhumka-sunsari",
+    features: [
+      {
+        name: "PM10",
+        code: 679
+      },
+      {
+        name: "PM2.5",
+        code: 681
+      },
+      {
+        name: "Wind Speed",
+        code: 682
+      },
+      {
+        name: "Wind Direction",
+        code: 683
+      },
+      {
+        name: "Total Suspended Particulate",
+        code: 696
+      },
+      {
+        name: "Air Temperature",
+        code: 697
+      },
+      {
+        name: "PM1",
+        code: 680
+      }
+    ]
+  },
+  {
+    name: "dhm-pokhara",
+    features: [
+      {
+        name: "PM10",
+        code: 597
+      },
+      {
+        name: "PM2.5",
+        code: 599
+      },
+      {
+        name: "Wind Speed",
+        code: 600
+      },
+      {
+        name: "Wind Direction",
+        code: 601
+      },
+      {
+        name: "Total Suspended Particulate",
+        code: 608
+      },
+      {
+        name: "Air Temperature",
+        code: 609
+      },
+      {
+        name: "PM1",
+        code: 598
+      }
+    ]
   }
 ];
 
