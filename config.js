263c43b764ee347faf13017f1f447dbefd0d1255
Nirmal Rijal <nirmalrijal41@gmail.com>
2019-04-18 12:52:30 +0545

    init

---
+++ config.js
@@ -0,0 +1,37 @@
+const dataSources = [
+  {
+    name: "simara-nepal",
+    features: [
+      {
+        name: "PM10",
+        code: 621
+      },
+      {
+        name: "PM2.5",
+        code: 622
+      },
+      {
+        name: "Wind Speed",
+        code: 623
+      },
+      {
+        name: "Wind Direction",
+        code: 624
+      },
+      {
+        name: "Total Suspended Particulate",
+        code: 631
+      },
+      {
+        name: "Air Temperature",
+        code: 632
+      },
+      {
+        name: "PM1",
+        code: 780
+      }
+    ]
+  }
+];
+
+module.exports = dataSources;
---
+++ index.js
@@ -0,0 +1,106 @@
+const request = require("request");
+const fs = require("fs");
+const path = require("path");
+
+const dataSources = require("./config");
+const maxRetentionDay = 5;
+
+main();
+
+async function main() {
+  createNewFolder(`data`);
+  for (let s = 0; s < dataSources.length; s += 1) {
+    const source = dataSources[s];
+    const history = getFetchHistoryList(source.name);
+    const historyFilePath = path.resolve(
+      __dirname,
+      `data/${source.name}/history.txt`
+    );
+    for (let i = 1; i <= maxRetentionDay; i += 1) {
+      const fetchDate = getNthPastDate(i);
+      if (history.indexOf(fetchDate) === -1) {
+        for (let f = 0; f < source.features.length; f += 1) {
+          const feature = source.features[f];
+          const dataFetchUrl = `http://pollution.gov.np/gss/api/observation?series_id=${
+            feature.code
+          }&date_from=${fetchDate}T00:00:00&date_to=${fetchDate}T24:00:00`;
+
+          const responseData = await fetchDataUsingRequest(dataFetchUrl);
+          saveJsonDataToFile(
+            responseData.data,
+            fetchDate,
+            source.name,
+            feature.name
+          );
+          console.log(`Complete for : ${fetchDate} - ${feature.name}`);
+        }
+        console.log(`Complete for date : ${fetchDate}`);
+        writeTextInFile(`${fetchDate}\n`, historyFilePath);
+      }
+    }
+  }
+  console.log("Complete");
+}
+
+function getFetchHistoryList(name) {
+  const filePath = path.resolve(__dirname, `data/${name}/history.txt`);
+  let historyList = [];
+  if (fs.existsSync(filePath)) {
+    const fileContent = fs.readFileSync(filePath, "utf8");
+    historyList = fileContent.split("\n");
+  } else {
+    createNewFolder(`data/${name}`);
+    fs.writeFileSync(filePath, "");
+  }
+  return historyList;
+}
+
+function getNthPastDate(n) {
+  const today = new Date();
+  const past = new Date(today);
+  past.setDate(today.getDate() - n);
+  const formattedDate = JSON.stringify(past)
+    .split("T")[0]
+    .replace('"', "");
+  return formattedDate;
+}
+
+function saveJsonDataToFile(data, date, name, feature) {
+  const filePath = path.resolve(
+    __dirname,
+    `data/${name}/${feature}/${date}.csv`
+  );
+  createNewFolder(`data/${name}/${feature}`);
+  fs.writeFileSync(filePath, `"datetime","value"\n`);
+  for (let i = 0; i < data.length; i += 1) {
+    const d = data[i];
+    let fileContent = `"${d.datetime}","${d.value}"\n`;
+    if (i === data.length - 1) {
+      fileContent = `"${d.datetime}","${d.value}"`;
+    }
+    writeTextInFile(fileContent, filePath);
+  }
+}
+
+function fetchDataUsingRequest(url) {
+  return new Promise((resolve, reject) => {
+    request(url, (error, response, body) => {
+      if (!error && response.statusCode == 200) {
+        resolve(JSON.parse(body));
+      } else {
+        reject("error");
+      }
+    });
+  });
+}
+
+function createNewFolder(p) {
+  const dirPath = path.resolve(__dirname, p);
+  if (!fs.existsSync(dirPath)) {
+    fs.mkdirSync(dirPath);
+  }
+}
+
+function writeTextInFile(text, filePath) {
+  fs.writeFileSync(filePath, text, { flag: "a" });
+}
